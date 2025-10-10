ef002333dc2422927e54052367363fde438cb486
devasya <devasyapatel555@gmail.com>
2025-10-10 23:38:45 +0530

    add model tweet

---
+++ app/Models/Tweet.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Tweet extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    // ... more code
+}
