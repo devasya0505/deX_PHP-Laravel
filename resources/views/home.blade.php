f1998e29f401928bc23e775c552140d3616a062b
devasya <devasyapatel555@gmail.com>
2025-10-11 23:07:01 +0530

    update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,23 @@
+
 <x-layout>
     <x-slot:title>
         Welcome
     </x-slot:title>
     <div class="max-w-2xl mx-auto">
-        @foreach ($tweets as $tweet)
+        @forelse ($tweets as $tweet)
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
                     <div>
-                        <div class="font-semibold">{{ $tweet['author'] }}</div>
-                        <div class="mt-1">{{ $tweet['message'] }}</div>
-                        <div class="text-sm text-gray-500 mt-2">{{ $tweet['time'] }}</div>
+                        <div class="font-semibold"> {{ $tweet->user ? $tweet->user->email : 'Anonymous' }}</div>
+                        <div class="mt-1">{{ $tweet->message }}</div>
+                        <div class="text-sm text-gray-500 mt-2">
+                            {{ $tweet->created_at->diffForHumans() }}
+                        </div>
                     </div>
                 </div>
             </div>
-        @endforeach
+        @empty
+            <p class="text-gray-500">No tweets yet. Be the first to tweet!</p>
+        @endforelse
     </div>
 </x-layout>
